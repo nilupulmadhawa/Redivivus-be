a503060653ed817409c6fb3d6c98c81fa3609d41
chamathjayasekara99 <it20654962@my.sliit.lk>
2022-08-30 11:07:22 +0530

    Add payment method funcitonality fully implemented

--- src/services/paymentmethod.js
+++ src/services/paymentmethod.js
@@ -1,4 +1,4 @@
-import { getAllPaymentMethods,insertPaymentMethod } from '../repository/paymentmethod'
+import { findPaymentMethod, getAllPaymentMethods,insertPaymentMethod } from '../repository/paymentmethod'
 
 
 export const retrieveAllPaymentMethods = async (page,limit) => {
@@ -6,7 +6,7 @@ export const retrieveAllPaymentMethods = async (page,limit) => {
 }
 
 export const createPaymentMethod = async (data) => {
-  const Employee = await findEmployee({ name: data.name })
-  if (Employee) return { status: 400, message: 'Employee name already taken' }
-  return await insertPaymentMethod({ ...data, creator: user._id })
+  const PaymentMethod = await findPaymentMethod({ cardNumber: data.cardNumber })
+  if (PaymentMethod) return { status: 400, message: 'This payment method  already added' }
+  return await insertPaymentMethod({ ...data})
 }
