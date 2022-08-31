2d9878d3fbc2a278dd8d325d3590dd322d6b8a41
chamathjayasekara99 <it20654962@my.sliit.lk>
2022-08-31 10:49:44 +0530

    Payment method CRUD funcitonalities fully implemented

--- src/repository/paymentmethod.js
+++ src/repository/paymentmethod.js
@@ -20,3 +20,43 @@ export const insertPaymentMethod = async (data) => {
 export const findPaymentMethod = async (filters) => {
   return await PaymentMethod.findOne(filters)
 }
+
+export const findAndUpdatePaymentMethod=async (filters,data) => {
+  //to return the  new document after updating is completed use set new= true
+
+
+    const {expirationDate,activeStatus,paymentAddress,cardNumber,cvc,postalCode} =data
+    //must include await
+    let paymentMethod= await findPaymentMethod(filters);
+    console.log(paymentMethod)
+    if(expirationDate!=null){
+      paymentMethod.expirationDate=expirationDate
+
+    }
+    if(activeStatus!=null){
+            paymentMethod.activeStatus=activeStatus
+
+    }
+    if(paymentAddress!=null){
+            paymentMethod.paymentAddress=paymentAddress
+
+    }
+    if(cardNumber!=null){
+      paymentMethod.cardNumber=cardNumber
+    }
+    if(cvc!=null){
+           paymentMethod.cvc=cvc
+
+    }
+    if(postalCode!=null){
+            paymentMethod.postalCode=postalCode
+
+    }
+    return await paymentMethod.save()
+}
+
+export const findAndDeletePaymentMethod =async (filters) => {
+  //must include await
+  let paymentMethod= await findPaymentMethod(filters);
+  return await PaymentMethod.deleteOne(paymentMethod)
+}
