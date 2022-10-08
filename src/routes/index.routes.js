76876159eb3f5165ccca9eb18d0e30bc36a6a77d
chamath.jayasekara <chamath.jayasekara@h-twon-tech.com>
2022-10-08 23:22:15 +0530

    routes -> router changed on index.routes file

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,18 +5,13 @@ import paymentMethodRouter from './payment_method.routes';
 import contactUsRouter from './contactus.routes';
 import pickupRequest from './pickupRequest.routes';
 
-const routes = express.Router()
+const router = express.Router()
 
-routes.use('/auth' , authRouter)
-routes.use('/user' , userRouter)
-routes.use('/paymentmethod' , paymentMethodRouter )
-routes.use('/contactus' , contactUsRouter )
+router.use('/auth' , authRouter)
+router.use('/user' , userRouter)
+router.use('/paymentmethod' , paymentMethodRouter )
+router.use('/contactus' , contactUsRouter )
 
-export default routes
-
-
-router.use('/paymentmethod', paymentMethodRouter)
-router.use('/contactus', contactUsRouter)
 router.use('/pickupRequest', pickupRequest)
 export default router;
 
