073cd08abc83595a6227a94c7986055f38a32743
chamath.jayasekara <chamath.jayasekara@h-twon-tech.com>
2022-10-09 15:18:24 +0530

    conflicts solved

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -11,7 +11,7 @@ const router = express.Router()
 router.use('/auth' , authRouter)
 router.use('/user' , userRouter)
 router.use('/paymentmethod' , paymentMethodRouter )
-<
+
 
 router.use('/contactus' , contactUsRouter )
 
