97b942849f475350f329a0b782e8653ff5fefb83
Nilupul Madhawa <dnmadawa@gmail.com>
2022-10-09 19:55:41 +0530

    Update index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,18 +5,16 @@ import paymentMethodRouter from './payment_method.routes';
 import contactUsRouter from './contactus.routes';
 import pickupRequest from './pickupRequest.routes';
 
-const routes = express.Router()
+const router = express.Router()
 
-routes.use('/auth' , authRouter)
-routes.use('/user' , userRouter)
-routes.use('/paymentmethod' , paymentMethodRouter )
-routes.use('/contactus' , contactUsRouter )
-
-export default routes
-
-
+router.use('/auth', authRouter)
+router.use('/user', userRouter)
 router.use('/paymentmethod', paymentMethodRouter)
+
+
 router.use('/contactus', contactUsRouter)
+
 router.use('/pickupRequest', pickupRequest)
+
 export default router;
 
